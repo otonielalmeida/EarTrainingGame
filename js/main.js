690e6959872e0404ea98b35e80c59efbf47fa40f
OTONIEL EDWARDS DE ALMEIDA <otonieledward@gmail.com>
2022-05-02 15:03:54 -0300

    Listen Again button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 button_keys = document.querySelector('.button-section');
 result = document.querySelector('.result');
 play_note = document.querySelector('.play_note');
+listenAgain = document.querySelector('.listen_again');
 
 let audioList = ['keys/A2.mp3', "keys/A3.mp3", "keys/A4.mp3", "keys/A5.mp3", "keys/A6.mp3",
                 'keys/B2.mp3', "keys/B3.mp3", "keys/B4.mp3", "keys/B5.mp3", "keys/B6.mp3",
@@ -13,32 +14,40 @@ let audioList = ['keys/A2.mp3', "keys/A3.mp3", "keys/A4.mp3", "keys/A5.mp3", "ke
 
 var myApp = {};
 
-play_note.onclick = function() {
+play_note.onclick = function () {
     const random = Math.floor(Math.random() * audioList.length);
     var audio = new Audio(audioList[random]);
+    myApp.listenAgain = audio;
+
     audio.play();
     let keyName = audio.src.split('/');
     keyName = keyName[4].split('.');
+    //nome da nota musical
     myApp.keyName = keyName[0];
-    console.log(keyName[0]);    
 }
-
-document.addEventListener('click', function(e){
+listenAgain.onclick = function () {
+    var audio = new Audio(myApp.listenAgain.src);
+    audio.play();
+}
+document.addEventListener('click', function (e) {
     const el = e.target;
-    myApp.keyName = myApp.keyName.split('');
-    myApp.keyName = myApp.keyName[0];
-    
+    myApp.fullKeyName = myApp.keyName;
+    myApp.shortKeyName = myApp.keyName.split('');
+    myApp.shortKeyName = myApp.keyName[0];
+
     if (el.innerText == 'A' || el.innerText == 'B'
-    || el.innerText == 'C' || el.innerText == 'D'
-    || el.innerText == 'E' || el.innerText == 'F' 
-    || el.innerText == 'G'){
-        if(el.innerText == myApp.keyName){
-            result.innerHTML = 'correct!';
-        } else if(el.innerText != myApp.keyName){
+        || el.innerText == 'C' || el.innerText == 'D'
+        || el.innerText == 'E' || el.innerText == 'F'
+        || el.innerText == 'G') {
+        if (el.innerText == myApp.shortKeyName) {
+            result.innerHTML = 'correct! ' + myApp.fullKeyName;
+            result.classList.add('correct');
+        } else if (el.innerText != myApp.shortKeyName) {
             result.innerHTML = 'incorrect!';
+            result.classList.remove('correct');
         } else {
             return;
         }
     }
-    
+
 })
